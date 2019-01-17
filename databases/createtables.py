098358a171e1ed341feef76c366bb643545ee8ed
b0nbon1 <b0nbon@yahoo.com>
2019-01-17 07:41:17 +0300

    [chore #163279587] creating tables

---
+++ databases/createtables.py
@@ -0,0 +1,21 @@
+users_table = ''' CREATE TABLE IF NOT EXISTS users (
+                id integer PRIMARY KEY,
+                public_id VARCHAR (100),
+                firstname VARCHAR (30) NOT NULL,
+                lastname VARCHAR (30) NOT NULL,
+                othername VARCHAR (30) NOT NULL,
+                username VARCHAR (30) UNIQUE NOT NULL,
+                registered DATE NOT NULL,
+                email VARCHAR (30) UNIQUE NOT NULL,
+                PhoneNumber VARCHAR NOT NULL,
+                isAdmin BOOLEAN NOT NULL DEFAULT FALSE,
+                password VARCHAR (200) NOT NULL
+                
+            );
+            '''
+
+queries = [users_table]
+
+"""Destroying tables for the test database"""
+table_users = ''' DROP TABLE IF EXISTS users CASCADE '''
+tablequeries = [table_users]

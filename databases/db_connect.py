8a48e06218ef8bd520363c04eaf09054291eb2ec
b0nbon1 <b0nbon@yahoo.com>
2019-01-17 07:41:06 +0300

    [chore #163279587] creating connwction to db

---
+++ databases/db_connect.py
@@ -0,0 +1,39 @@
+import psycopg2
+from createtables import queries, tablequeries
+db_url = "user='postgres' password='bonbon' host='127.0.0.1' port='5432' dbname='questioner'"
+
+
+def connect_to_db():
+    """making a connection to the db"""
+    try:
+        conn = psycopg2.connect(db_url)
+        print('databases connected')
+        return conn
+
+    except (Exception, psycopg2.Error) as error:
+        print("Not unable to connect to the database", error)
+
+
+def create_tables():
+    """creating tables for the database"""
+    try:
+        conn = connect_to_db()
+        cursor = conn.cursor()
+        for query in queries:
+            cursor.execute(query)
+        conn.commit()
+        print("successful created")
+    except (Exception, psycopg2.Error) as error:
+        print("Not unable to create tables", error)
+
+def destroy_tables():
+    """Destroying tables for the test database"""
+    try:
+        conn = connect_to_db()
+        cursor = conn.cursor()
+        for table in tablequeries:
+            cursor.execute(table)
+        conn.commit()
+        print("successful destroyed")
+    except (Exception, psycopg2.Error) as error:
+        print("Not unable to destroy tables", error)

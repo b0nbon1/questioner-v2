a6110c6563cd0b6168e5bed48371fcf528c0044d
b0nbon1 <b0nbon@yahoo.com>
2019-01-22 14:17:07 +0300

    [feature #163242572] add rsvps

--- app/api/v2/models/model_meetups.py
+++ app/api/v2/models/model_meetups.py
@@ -36,6 +36,24 @@ class Meetup(object):
             return {'error': '{}'.format(error)}, 401
 
     @staticmethod
+    def delete_meetup(meetup_id):
+        try:
+            conn = init_db()
+            cursor = conn.cursor()
+            # delete single record now
+            sql_delete_query = """Delete from meetups where id = %s"""
+            cursor.execute(sql_delete_query, (meetup_id, ))
+            conn.commit()
+            count = cursor.rowcount
+            if count == 0:
+                return jsonify({"error": "no such available meetup right now",
+                                      "status": 404}), 404
+            return jsonify({"data": count,
+                            "message": "meetup deleted successfully "}),200
+        except (Exception, psycopg2.Error) as error:
+            return jsonify({'error': '{}'.format(error)}), 401
+
+    @staticmethod
     def get_meetup():
         try:
             conn = init_db()
@@ -48,3 +66,22 @@ class Meetup(object):
             return meetup
         except(Exception, psycopg2.DatabaseError) as error:
             return {'error': '{}'.format(error)}, 401
+
+    @staticmethod
+    def new_rsvp(status, meetup_id, user_id):
+        rsvp = {
+            "user_id": user_id,
+            "meetup_id": meetup_id,
+            "status": status
+            }
+
+        try:
+            query = """INSERT INTO rsvps (user_id, meetup_id, status)
+                    VALUES (%(user_id)s, %(meetup_id)s, %(status)s) """
+            con = init_db()
+            cur = con.cursor()
+            cur.execute(query, rsvp)
+            con.commit()
+            return jsonify({"message": "rsvp successfully created"}), 201
+        except(Exception, psycopg2.DatabaseError) as error:
+            return jsonify({'error': '{}'.format(error)}), 400

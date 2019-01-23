0d0ad87e78f431ae3a5b3c516cfda5290f4e1c65
b0nbon1 <b0nbon@yahoo.com>
2019-01-23 09:37:51 +0300

    [feature #163242295] tests for questions

---
+++ tests/test_questions.py
@@ -0,0 +1,35 @@
+import pytest
+from flask import json
+
+
+def test_create_question(questions):
+    response = questions.Create_question()
+
+    assert response.status_code == 201
+
+
+def test_voteup(questions):
+    response = questions.vote('/api/v1/question/upvote/1')
+
+    assert response.status_code == 200
+
+
+def test_votedown(questions):
+    questions.Create_question()
+    response = questions.vote('/api/v1/question/downvote/2')
+
+    assert response.status_code == 200
+
+
+def test_fail_votedown(questions):
+    questions.Create_question()
+    response = questions.vote('/api/v1/question/downvote/5')
+
+    assert response.status_code == 404
+
+
+def test_fail_voteup(questions):
+    questions.Create_question()
+    response = questions.vote('/api/v1/question/upvote/5')
+
+    assert response.status_code == 404

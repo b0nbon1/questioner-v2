731c130b469d9c84f27cad7c8b8057f5016564d1
B0nbon1 <nyabuyabonvic@gmail.com>
2019-02-25 09:49:46 +0300

    views question

--- app/api/v2/views/questions.py
+++ app/api/v2/views/questions.py
@@ -1,7 +1,7 @@
 from ..models.questions_models import Questions
 from flask import Flask, jsonify, make_response, request, Blueprint
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from ..models.model_users import Users
+from ..models.users_models import User
 
 
 question = Blueprint('questions', __name__, url_prefix='/api/v2/questions')
@@ -19,39 +19,40 @@ def ask_question():
     except:
         return jsonify({"error": "enter json data"}, 400)
 
-    ask = Questions().create_question(user, meetup, title, body)
+    ask = Questions(user, meetup, title, body)
+    add_question = ask.create_question()
+    return make_response(jsonify({"data": add_question,
+                                    "message": "question successful created!",
+                                    "status": 201})), 201
 
 
-@question.route('/upvote/<int:question_id>', methods=['PATCH'])
-@jwt_required
-def vote_up(question_id):
-    question = [
-            question for question in questions if question['id'] == question_id]
-    if len(question) == 0:
-            return make_response(jsonify({"error": "no available questions right now",
-                                      "status": 404})), 404
-    user = get_jwt_identity()
 
-    votes = Questions().add_vote(question_id, user_id)
-    return votes
+# @question.route('/upvote/<int:question_id>', methods=['PATCH'])
+# @jwt_required
+# def vote_up(question_id):
+    
+#     user = get_jwt_identity()
+
+#     votes = Questions.add_vote(question_id, user)
+#     return votes
 
 
-@question.route('/downvote/<int:question_id>', methods=['PATCH'])
-@jwt_required
-def vote_down(question_id):
-    question = [
-            question for question in questions if question['id'] == question_id]
-    if len(question) == 0:
-            return make_response(jsonify({"error": "no available questions right now",
-                                      "status": 404})), 404
-    user = get_jwt_identity()
-    user_id = [u for u in Users if u['public_id'] == user][0]['id']
-    votes = Questions().add_vote(question_id, user_id)
-    if votes is False:
-        return make_response(jsonify({"message": "you have already voted",
-                                      "status": 400})), 400
-    vote = votes[0]
-    vote['downvotes'] = vote['downvotes'] + 1
+# @question.route('/downvote/<int:question_id>', methods=['PATCH'])
+# @jwt_required
+# def vote_down(question_id):
+#     question = [
+#             question for question in questions if question['id'] == question_id]
+#     if len(question) == 0:
+#             return make_response(jsonify({"error": "no available questions right now",
+#                                       "status": 404})), 404
+#     user = get_jwt_identity()
+#     user_id = [u for u in Users if u['public_id'] == user][0]['id']
+#     votes = Questions().add_vote(question_id, user_id)
+#     if votes is False:
+#         return make_response(jsonify({"message": "you have already voted",
+#                                       "status": 400})), 400
+#     vote = votes[0]
+#     vote['downvotes'] = vote['downvotes'] + 1
 
-    return make_response(jsonify({'status': 200,
-                                  'data': vote})), 200
+#     return make_response(jsonify({'status': 200,
+#                                   'data': vote})), 200

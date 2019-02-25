3906d128f733f5fa959670026e1f5a0e966fb3ae
B0nbon1 <nyabuyabonvic@gmail.com>
2019-02-25 09:48:53 +0300

    adds views questions

--- app/api/v2/models/questions_models.py
+++ app/api/v2/models/questions_models.py
@@ -1,6 +1,8 @@
 from flask import Flask, abort, jsonify, make_response, request
 from datetime import datetime
+from databases.db_connect import init_db
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 
 class Questions():
@@ -12,14 +14,14 @@ class Questions():
 
     def create_question(self):
         new_question = {
-            "user": self.user,
+            "user_id": self.user,
             "meetup": self.meetup,
             "title": self.title,
             "body": self.body,
         }
         try:
-            query = """INSERT INTO questions(user, meetup, title, body)
-                    VALUES (%(user)s, %(meetup)s, %(title)s, %(body)s) """
+            query = """INSERT INTO questions(user_id, meetup, title, body)
+                    VALUES (%(user_id)s, %(meetup)s, %(title)s, %(body)s) """
             con = init_db()
             cur = con.cursor()
             cur.execute(query, new_question)
@@ -42,26 +44,6 @@ class Questions():
         except(Exception, psycopg2.DatabaseError) as error:
             return {'error': '{}'.format(error)}, 401
 
-    @classmethod
-    def add_vote(cls, question_id, user):
-        try:
-            conn = init_db()
-            cur = conn.cursor(cursor_factory=RealDictCursor)
-            cur.execute("select * from votes", ())
-            votes = cur.fetchall()
-            cur.close()
-            conn.close()
-        except(Exception, psycopg2.DatabaseError) as error:
-            return {'error': '{}'.format(error)}, 401
-
-        if len(votes) == 0:
-            cls.vote(user, question_id)
-        else:
-            vote_question = [v for v in votes if v['question_id'] == question_id]
-            if len(v) == 0:
-               
-                return votes
-
     @staticmethod
     def vote(user, question_id):
         new_vote = {
@@ -81,3 +63,35 @@ class Questions():
             return new_question
         except(Exception, psycopg2.DatabaseError) as error:
             return {'error': '{}'.format(error)}, 400
+
+    @classmethod
+    def add_vote(cls, question_id, user):
+        quest = Questions.get_question()
+        # return {'error':quest}
+        question = [
+                question for question in quest if question['id'] == question_id]
+        if len(question) == 0:
+                return make_response(jsonify({"error": "no available questions right now",
+                                        "status": 404})), 404
+        try:
+            conn = init_db()
+            cur = conn.cursor(cursor_factory=RealDictCursor)
+            cur.execute("select * from votes", ())
+            votes = cur.fetchall()
+            cur.close()
+            conn.close()
+        except(Exception, psycopg2.DatabaseError) as error:
+            return {'error': '{}'.format(error)}, 401
+
+        if len(votes) == 0:
+            return cls.vote(user, question_id)
+        else:
+            vote_question = [v for v in votes if v['question_id'] == question_id]
+            if len(vote_question) == 0:
+                return cls.vote(user, question_id)
+            else:
+                voter = vote_question[0]
+                if voter is None:
+                    return cls.vote(user, question_id)
+                else:
+                    pass
